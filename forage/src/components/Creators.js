0669ae10ab88d87d21edb8745af826a069b0482f
Bill Gan <billgan12345@gmail.com>
2020-12-30 14:01:22 -0500

    Updated descriptions

--- forage/src/components/Creators.js
+++ forage/src/components/Creators.js
@@ -8,7 +8,7 @@ export const Creators = () => {
             <h1>Meet the Team!</h1>
             <div class="team-members">
                 <Member name="Leo Tian">
-
+                Leo Tian is an aspiring engineering or CS major! He is currently developing competencies in several coding languages such as python, C, HTML, CSS, and Javascript, as well as React.
                 </Member>
 
                 <Member name="Pak Tung Chow">
@@ -16,11 +16,11 @@ export const Creators = () => {
                 </Member>
                 
                 <Member name="Bill Gan">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum elementum arcu, a mollis quam porta quis. Donec ligula elit, sodales sed ante sed, tincidunt convallis magna.
+                Bill Gan is a Grade 12 student who is looking to pursue a career in software engineering. He is currently learning React, Firebase, JS/HTML/CSS, and Discord.js. He also likes developing games with Gamemaker Studio: 2.
                 </Member>
                 
                 <Member name="Fiona">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum elementum arcu, a mollis quam porta quis. Donec ligula elit, sodales sed ante sed, tincidunt convallis magna.
+                Fiona Xie plans on pursing engineering and enjoys playing around with all sorts of technology. She is self-teaching herself Python, HTML, CSS and Javascript at the moment but has fundamental literacy in Java as well. She also plays bedwars on Minecraft way too much.
                 </Member>
             </div>
         </section>
